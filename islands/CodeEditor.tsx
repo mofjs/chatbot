6f4a86028d8cd53da68bfd258a86520b61397cbf
Sukirno <mblonyox@gmail.com>
2024-01-23 11:46:16 +0700

    fix: CodeEditor typescript definition.

--- islands/CodeEditor.tsx
+++ islands/CodeEditor.tsx
@@ -1,11 +1,15 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { createRef, JSX } from "preact";
 import { useEffect } from "preact/hooks";
-import { Require } from "https://esm.sh/v135/@types/requirejs@2.1.37/index.d.ts";
+
+// *.d.ts
+import Require from "https://esm.sh/v135/@types/requirejs@2.1.37/index.d.ts";
 import Monaco from "https://esm.sh/v135/monaco-editor@0.45.0/esm/vs/editor/editor.api.d.ts";
 
 declare const require: Require;
 declare const monaco: typeof Monaco;
+
+const DTS_BASE_URL = `https://deno.land/x/deno@v1.39.4/cli/tsc/dts/`;
 
 type Props = JSX.HTMLAttributes<HTMLTextAreaElement> & {
   language: "json" | "typescript";
@@ -15,7 +19,9 @@ export default function CodeEditor(props: Props) {
   if (!IS_BROWSER) {
     return (
       <>
-        <div />
+        <div>
+          <div />
+        </div>
         <textarea {...props} />
       </>
     );
@@ -25,11 +31,29 @@ export default function CodeEditor(props: Props) {
   useEffect(() => {
     if (divEl.current) {
       require.config({ paths: { vs: "/vs" } });
-      require(["vs/editor/editor.main"], () => {
+      require(["vs/editor/editor.main"], async () => {
+        if (props.language === "typescript") {
+          const libs = [
+            "lib.deno.ns.d.ts",
+            "lib.dom.d.ts",
+            "lib.dom.extras.d.ts",
+          ];
+
+          for await (
+            const content of libs.map((lib) =>
+              fetch(new URL(lib, DTS_BASE_URL)).then((r) => r.text())
+            )
+          ) {
+            monaco.languages.typescript.typescriptDefaults.addExtraLib(
+              content,
+            );
+          }
+        }
         const editor = monaco.editor.create(divEl.current!, {
           value: (props.value ?? props.defaultValue)?.toString(),
           language: props.language,
           theme: "vs-dark",
+          formatOnType: true,
         });
         const updateValue = () => {
           if (textareaEl.current) {
@@ -41,10 +65,12 @@ export default function CodeEditor(props: Props) {
         updateValue();
       });
     }
-  }, []);
+  }, [divEl.current]);
   return (
     <>
-      <div ref={divEl} className="monaco-editor-container" />
+      <div className="monaco-editor-container">
+        <div ref={divEl} />
+      </div>
       <textarea
         ref={textareaEl}
         {...props}
